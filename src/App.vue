bf4ecac07c4e1f346df959b575d2208a1d94ab10
Maciej Hryniszak <mhryn@softserveinc.com>
2017-10-26 16:48:15 +0200

    Initial import

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data () {
+    return {
+      message: 'Hello, world!'
+    }
+  }
+})
+</script>
